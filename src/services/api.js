809b2107b713e016622c451e6e676cc24de1bb81
JoeDlc15 <Limache2306@outlook.com>
2026-03-10 05:55:05 -0500

    Update API base URL to production endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,6 @@
-const API_BASE_URL = 'http://localhost:3000/api';
+//const API_BASE_URL = 'http://localhost:3000/api';
 //const API_BASE_URL = 'https://bitter-zebras-think.loca.lt/api';
+const API_BASE_URL = 'https://backend-volper.onrender.com/api';
 
 export const getProducts = async () => {
     try {
